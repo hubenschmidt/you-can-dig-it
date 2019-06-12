b803ec140a405dc2898fc94fc529accb95674a32
hubenschmidt <whubenschmidt@gmail.com>
2019-06-12 05:56:48 -0400

    building front end with react

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+    render(){
+        return (
+            <div className="navbar-fixed">
+                
+            </div>
+        )
+    }
+}
