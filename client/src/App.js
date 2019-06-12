1b92995103117b4edf6dd8d3a7687ee18eb9e747
hubenschmidt <whubenschmidt@gmail.com>
2019-06-12 09:24:55 -0400

    instructions to devs

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>you-can-dig-it</h1>
+        <h2>this is a front end made with React</h2>
+        <h3>npm run dev</h3>
+        <h4>Client on [http://localhost:3000](http://localhost:3000)</h4>
+        <h4>Server on [http://localhost:5000](http://localhost:5000)</h4>
       </div>
     );
   }
