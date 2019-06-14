d37b8d123ee08f3ee095e86e169b67a361e372ad
hubenschmidt <whubenschmidt@gmail.com>
2019-06-14 03:10:02 -0400

    refactor local authentication to localAuth Controller, clean up API routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import React, {Component} from 'react';
-import './App.css';
+// // import React, {Component} from 'react';
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <h1>you-can-dig-it</h1>
-        <h2>this is a front end made with React</h2>
-        <h3>npm run dev</h3>
-        <h4>Client on [http://localhost:3000](http://localhost:3000)</h4>
-        <h4>Server on [http://localhost:5000](http://localhost:5000)</h4>
-      </div>
-    );
-  }
-}
-export default App;
+// import './App.css';
+
+// class App extends Component {
+//   // render(){
+//   //   // return (
+//   //   //   // <div className="App">
+//   //   //   //   <h1>you-can-dig-it</h1>
+//   //   //   //   <h2>this is a front end made with React</h2>
+//   //   //   //   <h3>npm run dev</h3>
+//   //   //   //   <h4>Client on [http://localhost:3000](http://localhost:3000)</h4>
+//   //   //   //   <h4>Server on [http://localhost:5000](http://localhost:5000)</h4>
+//   //   //   // </div>
+//   //   // );
+//   // }
+// }
+// export default App;
