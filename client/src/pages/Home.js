b1a9105daac9f5cf17af9b0a0f5191631facdb4a
Iwona Antonik <iwona.antonik@gmail.com>
2019-06-14 15:19:23 -0400

    client routes, library carousel wit static images, NoMatch page -- Work in progress

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+    return (
+
+        <div className="App" >
+            <h1>you-can-dig-it</h1>
+            <h2>this is a front end made with React</h2>
+            <h3>npm run dev</h3>
+            <h4>Client on [http://localhost:3000](http://localhost:3000)</h4>
+            <h4>Server on [http://localhost:5000](http://localhost:5000)</h4>
+        </div >
+
+    )
+}
+
+export default Home;
