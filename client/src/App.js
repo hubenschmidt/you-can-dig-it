85a69631c47007771b3d5d8c7d110eb39f9662d7
hubenschmidt <whubenschmidt@gmail.com>
2019-06-14 22:46:12 -0400

    working registration form at /register using React

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
 
 import './App.css';
 
 const App = () => 
-    <div className="App">
-    <BrowserRouter>
-          <Navbar />
-          <Landing />
-        <h1>you-can-dig-it</h1>
-        <h2>this is a front end made with React</h2>
-        <h3>npm run dev</h3>
-        <h4>Client on [http://localhost:3000](http://localhost:3000)</h4>
-        <h4>Server on [http://localhost:5000](http://localhost:5000)</h4>
-    </BrowserRouter>
-      </div>
+    <Router>
+        <div className='App'>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/register' component={Register} />
+            {/* <Router exact path='/login' component={Login} /> */}
+        </div>
+    </Router>
 
 
 export default App;
