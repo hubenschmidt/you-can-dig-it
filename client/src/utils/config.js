a13812bbba3df40248859fb16b7744c93975231e
hubenschmidt <whubenschmidt@gmail.com>
2019-06-27 23:12:45 -0400

    fix API_URL

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,4 +1,4 @@
 export const API_URL = process.env.NODE_ENV === 'production'
 // ? '/'
-? 'https://young-mesa-54357.herokuapp.com/'
+? 'https://young-mesa-54357.herokuapp.com'
 : 'https://localhost:5000'
