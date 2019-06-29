afd8d774158fac5e8af03f73ced6392b69410df0
hubenschmidt <whubenschmidt@gmail.com>
2019-06-29 08:46:52 -0400

    final commit

--- client/src/components/BrandDesc/index.js
+++ client/src/components/BrandDesc/index.js
@@ -22,9 +22,10 @@ class BrandDesc extends Component {
                 <div className="brand-card" >
                     <a href="#" className='brand-card-close' ></a>
                         <div className="brand-content">
-                        <h1 className="brand-text">connect your discogs account</h1>
-                        <h1 className="brand-text">hear your collection</h1>
-                        <h1 className="brand-text">ad free</h1>
+                        <h1 style={{color:'white'}}>dc3: digitalCrates</h1>
+                        <h2 className="brand-text">connect your discogs account</h2>
+                        <h2 className="brand-text">hear your collection</h2>
+                        <h2 className="brand-text">ad free</h2>
                         </div>
                     </div>
             </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
 
         return (
             <Container>
-            {/* <BrandDesc /> */}
+            <BrandDesc />
             </Container>
         );
     }
